a9a42613fce0ffae0a8a60f93072d4e186346f27
renzoyzz <renzoyzevallos@gmail.com>
2020-06-22 19:01:20 -0700

    lobby setup

--- bot.py
+++ bot.py
@@ -1,6 +1,28 @@
+from enum import Enum
 import discord
 
 client = discord.Client()
+
+class Stage(Enum):
+    IDLE = 0
+    SETUP_CHANNELS = 1
+    REGISTER_PLAYERS = 2
+    SPLIT = 3
+
+
+class Lobby:
+    players = []
+    stage = Stage.IDLE
+    channelId = 0
+    teamOneChannel = 0
+    teamTwoChannel = 0
+
+class Player:
+    def __init__(self, username, id):
+        self.username = username
+        self.id = id
+    
+lobby = Lobby()
 
 
 @client.event
@@ -10,8 +32,23 @@ async def on_ready():
 @client.event
 async def on_message(message):
     print(message)
-    if message.content.startswith('!'):
-        await message.channel.send('Hello')
+    print(lobby.stage)
+    if lobby.stage == Stage.IDLE and message.content.startswith('!Teamster'):
+        await message.channel.send('Players register by typing !register')
+        lobby.channelId = message.channel.id
+        lobby.stage = Stage.REGISTER_PLAYERS
+    if lobby.stage == Stage.REGISTER_PLAYERS and message.content.startswith('!register'):
+        player = Player(message.author.name, message.author.id)
+        lobby.players.append(player)
+        await message.channel.send('{0} has been registered'.format(player.username))
+    if lobby.stage == Stage.REGISTER_PLAYERS and message.content.startswith('!list'):
+        for p in lobby.players:
+            await message.channel.send(p.username)
+    if lobby.stage == Stage.REGISTER_PLAYERS and message.content.startswith('!lockin'):
+        for p in lobby.players:
+            await message.channel.send(p.username)
 
 
-client.run('NzI0Nzc0NDExNTg4MjA2NjQz.XvFL1Q.WjdVAqdUg4BZDc_HLgil3o1Hrgs')
+
+
+client.run('')
