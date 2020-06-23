463983b7ddda1f31ee1367455a89cb91b44ffd3a
kittipaw <tiamsuwan@gmail.com>
2020-06-23 10:53:27 -0400

    Add ability to remove yourself from the list, handled duplicates in player list, add ability to customize channels for teams

--- bot.py
+++ bot.py
@@ -8,8 +8,40 @@ config = configparser.ConfigParser()
 config.read('config.ini')
 print('The token is {}'.format(config['DEFAULT']['token']))
 token = config['DEFAULT']['token']
+tside = config['DEFAULT']['tside']
+ctside = config['DEFAULT']['ctside']
 
 client = discord.Client()
+
+def configure_team_channel(message):
+    global lobby
+    global tside
+    global ctside
+    inputs = message.content.split()
+    print(inputs)
+    if(len(inputs) != 3):
+        return("Invalid number of arguments, please supply only 2 channels")
+    else:
+        lobby1 = inputs[1]
+        lobby2 = inputs[2]
+        foundLobby1 = False
+        foundLobby2 = False
+        for c in message.channel.guild.voice_channels:
+            if c.name == lobby1:
+                foundLobby1 = True
+            if c.name == lobby2:
+                foundLobby2 = True
+        if(foundLobby1 & foundLobby2):
+            tside = lobby1
+            ctside = lobby2
+            return("T-Side is now {}, CT-Side is now {}".format(lobby1,lobby2))
+        else:
+            if(foundLobby1 & (not foundLobby2)):
+                return("{} is not found on server's list of channels".format(lobby2))
+            elif(foundLobby2 & (not foundLobby1)):
+                return("{} is not found on server's list of channels".format(lobby1))
+            else:
+                return("Supplied channels do not exist, not updating")
 
 class Stage(Enum):
     IDLE = 0
@@ -19,7 +51,7 @@ class Stage(Enum):
 
 
 class Lobby:
-    players = []
+    players = set()
     stage = Stage.IDLE
     startingChannel = 0 
     teamOneChannel = 0
@@ -44,18 +76,25 @@ async def on_message(message):
         await message.channel.send('Players register by typing !register')
         lobby.stage = Stage.REGISTER_PLAYERS
     elif lobby.stage == Stage.REGISTER_PLAYERS and message.content.startswith('!register'):
-        lobby.players.append(message.author)
+        lobby.players.add(message.author)
         await message.channel.send('{0} has been registered'.format(message.author.name))
         await message.author.move_to(lobby.startingChannel)
+    elif lobby.stage == Stage.REGISTER_PLAYERS and message.content.startswith('!removeme'):
+        lobby.players.remove(message.author)
+        await message.channel.send('{0} has been removed'.format(message.author.name))
     elif lobby.stage == Stage.REGISTER_PLAYERS and message.content.startswith('!list'):
+        listOfPlayers = 'Current registered players : '
         for p in lobby.players:
-            await message.channel.send(p.name)
+            listOfPlayers += '\n' + p.name
+        await message.channel.send(listOfPlayers)
+    elif message.content.startswith('!configure'):
+        await message.channel.send(configure_team_channel(message))
     elif lobby.stage == Stage.REGISTER_PLAYERS and message.content.startswith('!lockin'):
         await message.channel.send("Moving players to random teams!")
         i = 0
         playerCount = len(lobby.players)
         while i < playerCount:
-            player = random.choice(lobby.players)
+            player = random.choice(tuple(lobby.players))
             lobby.players.remove(player)
             if i % 2 == 0:
                 lobby.teamOne.append(player)
@@ -63,12 +102,11 @@ async def on_message(message):
                 lobby.teamTwo.append(player)
             i += 1
         for c in message.channel.guild.voice_channels:
-           if c.name == 'T-Side':
+           if c.name == tside:
                 for p in lobby.teamOne:
                     await p.move_to(c)
-           elif c.name == 'CT-Side':
+           elif c.name == ctside:
                 for p in lobby.teamTwo:
                     await p.move_to(c)
 
 client.run(token)
-
